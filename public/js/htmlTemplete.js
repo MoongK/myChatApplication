17c3708ae6535446822648cf54e813c3d8463654
JungMunGyeong <dijiket@naver.com>
2020-01-31 18:02:17 +0900

    80%

--- public/js/htmlTemplete.js
+++ public/js/htmlTemplete.js
@@ -24,5 +24,33 @@ module.exports = {
       `;
       response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
       response.end(`${templete}`);
+    },
+    login:function(response, _list){
+        var list = _list;
+        var templete = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>오늘 점심 정하고 참가해주세요</title>
+            <link rel="stylesheet" href="../css/login.css">
+            <script src="http://192.168.10.27:3002/socket.io/socket.io.js"></script>
+            <script src="../js/login.js"></script>
+        </head>
+        <body>
+            <div class="homeDiv">
+                <h5>참여중인 유저..</h5>
+                <ul>
+                ${list}
+                </ul>
+                <h2>이름 입력만 하고 참가!</h2>
+                <form action="/startChat" method="POST" accept-charset="utf-8">            
+                    <input type="text" name="userName"><br>
+                    <input id="joinBtn" type="submit" value="들어간다!">
+                </form>
+            </div>
+        </body>
+</html>
+        `;
     }
   }
